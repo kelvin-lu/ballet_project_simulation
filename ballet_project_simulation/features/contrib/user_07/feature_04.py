f37d38e62961e8c6d4a9b775427c6a6b4f73f93a
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 22:46:45 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_07/feature_04.py
@@ -0,0 +1,10 @@
+from ballet import Feature
+from sklearn.impute import SimpleImputer
+
+# BsmtFinSF1, BsmtFinSF2, BsmtUnfSF, TotalBsmtSF, BsmtFullBath and BsmtHalfBath :
+# missing values are likely zero for having no basement
+
+input = ["Total Bsmt SF"]
+transformer = SimpleImputer(strategy="mean")
+name = "Imputed total square feet of basement area"
+feature = Feature(input=input, transformer=transformer)
