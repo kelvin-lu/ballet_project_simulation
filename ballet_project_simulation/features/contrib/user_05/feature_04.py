0b50720d897b7379022585759099867d09ea52c7
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 23:02:17 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_05/feature_04.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+import sklearn.impute
+
+
+input = ["Lot Frontage"]
+transformer = sklearn.impute.SimpleImputer(strategy="mean")
+name = "Lot Frontage Fill"
+feature = Feature(input=input, transformer=transformer, name=name)
