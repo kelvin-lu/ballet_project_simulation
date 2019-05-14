21dd2e2d2642703e5ab836dcd230b5670e15358c
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 23:06:55 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_09/feature_05.py
@@ -0,0 +1,13 @@
+from ballet import Feature
+from ballet.eng import SimpleFunctionTransformer
+
+input = "BsmtFin SF 2"
+
+
+def has_qual(ser):
+    return (ser > 0).astype(int)
+
+
+transformer = SimpleFunctionTransformer(has_qual)
+name = "Basement type 2 indicator"
+feature = Feature(input=input, transformer=transformer, name=name)
