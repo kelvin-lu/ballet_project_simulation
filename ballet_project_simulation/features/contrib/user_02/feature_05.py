91d8b40dd9ba9b69e85ec9ab39263d762365ff6f
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 22:54:01 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_02/feature_05.py
@@ -0,0 +1,12 @@
+from ballet import Feature
+import ballet.eng
+import sklearn.preprocessing
+
+
+input = ["Foundation"]
+transformer = [
+    ballet.eng.NullFiller(replacement="None"),
+    sklearn.preprocessing.OneHotEncoder(),
+]
+name = "Foundation Type"
+feature = Feature(input=input, transformer=transformer, name=name)
