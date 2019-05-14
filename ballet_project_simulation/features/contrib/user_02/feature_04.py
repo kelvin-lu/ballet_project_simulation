1b302e10d2f72de3dc7fd5635e62c96ce81fe860
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 22:06:54 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_02/feature_04.py
@@ -0,0 +1,12 @@
+from ballet import Feature
+import ballet.eng
+import sklearn.preprocessing
+
+
+input = ["Heating"]
+transformer = [
+    ballet.eng.NullFiller(replacement="None"),
+    sklearn.preprocessing.OneHotEncoder(),
+]
+name = "Heating Type"
+feature = Feature(input=input, transformer=transformer, name=name)
