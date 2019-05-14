c982b3e6b6b60e19052b9bc0d2518c8a41ded837
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 22:57:07 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_04/feature_03.py
@@ -0,0 +1,12 @@
+from ballet import Feature
+from ballet.eng import SimpleFunctionTransformer
+
+
+def calc_qual(df):
+    return df["Overall Qual"] - df["Overall Cond"]
+
+
+input = ["Overall Qual", "Overall Cond"]
+transformer = SimpleFunctionTransformer(calc_qual)
+name = "Qual"
+feature = Feature(input=input, transformer=transformer, name=name)
