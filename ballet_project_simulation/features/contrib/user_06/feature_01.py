a848724185bbe41e325869db83065179e456d317
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 20:03:02 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_06/feature_01.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from ballet.eng import NullFiller
+from sklearn.preprocessing import OneHotEncoder
+
+input = ["Pool QC"]
+transformer = [NullFiller(replacement="None"), OneHotEncoder()]
+name = "Pool quality type"
+feature = Feature(input=input, transformer=transformer, name=name)
