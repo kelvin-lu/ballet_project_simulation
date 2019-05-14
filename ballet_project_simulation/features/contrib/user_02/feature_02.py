7b12eea378eae37146d2b2cfa9b07a47f6d02122
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 21:26:20 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_02/feature_02.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+import sklearn.preprocessing
+
+
+input = ["Mo Sold"]
+transformer = sklearn.preprocessing.OneHotEncoder()
+name = "Month Sold Categorical"
+feature = Feature(input=input, transformer=transformer, name=name)
