4bf4002aebe27d9e3735477ffcd8af4116e4d98e
ballet-app <dai-lab@mit.edu>
2019-05-14 20:38:23 +0000

    Pruning Redundant Features: 02.01

--- ballet_project_simulation/features/contrib/user_02/feature_01.py
+++
@@ -1,8 +0,0 @@
-from ballet import Feature
-import sklearn.preprocessing
-
-
-input = ["Yr Sold"]
-transformer = sklearn.preprocessing.OneHotEncoder()
-name = "Year Sold Categorical"
-feature = Feature(input=input, transformer=transformer, name=name)
