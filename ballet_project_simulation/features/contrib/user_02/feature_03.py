7f5b4c5844bbaf1055eb0bb03fa7dd9a6e8ac11a
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 21:54:23 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_02/feature_03.py
@@ -0,0 +1,13 @@
+from ballet import Feature
+import ballet.eng
+import pandas as pd
+
+
+def fill_garage(df):
+    return df["Year Built"].where(pd.notnull, df["Garage Yr Blt"])
+
+
+input = ["Garage Yr Blt", "Year Built"]
+transformer = ballet.eng.SimpleFunctionTransformer(fill_garage)
+name = "Impute garage year built"
+feature = Feature(input=input, transformer=transformer, name=name)
