4a5660a58473567b7ad7ede3a7373d610a72bc86
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 20:44:33 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_05/feature_01.py
@@ -0,0 +1,11 @@
+from ballet import Feature
+import ballet.eng
+import sklearn.preprocessing
+
+input = ["Alley"]
+transformer = [
+    ballet.eng.NullFiller(replacement="NOACCESS"),
+    sklearn.preprocessing.OneHotEncoder(),
+]
+name = "Alley Misc Fill"
+feature = Feature(input=input, transformer=transformer, name=name)
