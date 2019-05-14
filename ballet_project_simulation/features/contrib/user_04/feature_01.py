9e4ef7a477853448d0d602396d157d978823cfc9
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 22:25:55 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_04/feature_01.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from ballet.eng import NullFiller, SimpleFunctionTransformer
+
+
+input = ["Total Bsmt SF", "1st Flr SF", "2nd Flr SF"]
+transformer = [SimpleFunctionTransformer(lambda df: df.sum(axis=1)), NullFiller()]
+name = "Total area calculated"
+feature = Feature(input=input, transformer=transformer, name=name)
