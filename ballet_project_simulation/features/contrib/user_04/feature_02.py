69913f9caf456c3b94428c9dfb96f82ab32b58cd
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 22:42:13 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_04/feature_02.py
@@ -0,0 +1,12 @@
+from ballet import Feature
+from ballet.eng import NullFiller, SimpleFunctionTransformer
+
+
+def calc_age(df):
+    return df["Yr Sold"] - df["Year Remod/Add"]
+
+
+input = ["Yr Sold", "Year Remod/Add"]
+transformer = [SimpleFunctionTransformer(calc_age), NullFiller()]
+name = "Years since remodeled"
+feature = Feature(input=input, transformer=transformer, name=name)
