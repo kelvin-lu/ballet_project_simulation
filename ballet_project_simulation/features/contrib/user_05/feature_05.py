dd053e0ee4d32e633191b15486111ea84afe69a2
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 23:14:36 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_05/feature_05.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from sklearn.preprocessing import OneHotEncoder
+
+
+input = ["Overall Cond"]
+transformer = OneHotEncoder()
+name = "Overall condition"
+feature = Feature(input=input, transformer=transformer, name=name)
