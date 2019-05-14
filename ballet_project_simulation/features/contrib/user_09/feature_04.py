92c798f1fc0757eed561b38b4cfca353b38e7af8
kelvin-lu <kelvinlu@mit.edu>
2019-05-14 22:31:50 +0000

    Add new feature

---
+++ ballet_project_simulation/features/contrib/user_09/feature_04.py
@@ -0,0 +1,8 @@
+from ballet import Feature
+from ballet.eng import NullFiller
+from sklearn.preprocessing import OneHotEncoder
+
+input = ["BsmtFin Type 2"]
+transformer = [NullFiller(replacement="None"), OneHotEncoder()]
+name = "Rating of basement finished area (if multiple types)"
+feature = Feature(input=input, transformer=transformer, name=name)
